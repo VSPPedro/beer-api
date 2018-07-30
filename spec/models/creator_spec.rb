e7524977a6ebc5bf17f46d4d7c399482bdc03879
Pedro Paiva <pedrovsp109@gmail.com>
2018-07-29 21:55:52 -0300

    Update Creator model validating presence of beer_id

--- spec/models/creator_spec.rb
+++ spec/models/creator_spec.rb
@@ -4,8 +4,10 @@ RSpec.describe Creator, type: :model do
   let(:creator) { build(:creator) }
   # Field test
   it { is_expected.to respond_to(:name) }
+  it { is_expected.to respond_to(:beer_id) }
   # Association test
   it { is_expected.to belong_to(:beer) }
   # Validation test
   it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:beer_id) }
 end
