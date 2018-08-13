1cefd01256bc99269c61399ed81968cf8a147c92
Pedro Paiva <pedrovsp109@gmail.com>
2018-08-13 13:13:09 -0300

    chore: Removing reject_if
    These fields are already validate for their respective models.

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -4,19 +4,7 @@ class Beer < ApplicationRecord
   has_many :beer_volumes
   has_many :volumes, through: :beer_volumes
   validates_presence_of :name
-  accepts_nested_attributes_for :creators, reject_if: :reject_creators
-  accepts_nested_attributes_for :tips, reject_if: :reject_tips
-  accepts_nested_attributes_for :volumes, reject_if: :reject_volumes
-
-  def reject_creators(attributes)
-    attributes['name'].blank?
-  end
-
-  def reject_tips(attributes)
-    attributes['description'].blank?
-  end
-
-  def reject_volumes(attributes)
-    attributes['unit'].blank? && attributes['value'].blank?
-  end
+  accepts_nested_attributes_for :creators
+  accepts_nested_attributes_for :tips
+  accepts_nested_attributes_for :volumes
 end
